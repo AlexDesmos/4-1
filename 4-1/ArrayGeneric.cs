b3a189850728ae26fb141aa8dd8e85bba17aad0e
AlexDesmos <a.m.borodai@gmail.com>
2024-03-25 13:41:38 +0300

    add and resize

---
+++ 4-1/ArrayGeneric.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_1
+{
+    class ArrayGeneric<P>
+    {
+        private P[] list;
+        private int capacity;
+        private int default_capacity = 7;
+        private int size = 0;
+        
+        public ArrayGeneric(int capacity)
+        {
+            this.capacity = capacity;
+            list = new P[capacity];
+        }
+        public ArrayGeneric()
+        {
+            this.capacity = default_capacity;
+            list = new P[default_capacity];
+        }
+        public void Add(P element)
+        {
+            if (size == capacity)
+            {
+                Resize(); 
+            }
+            list[size] = element;
+        }
+        public void Resize()
+        {
+            capacity = capacity * 2 + 1;
+            P[] _list = new P[capacity];
+            Array.Copy(list, _list, size);
+            list = _list;
+        }
+    }
+}
---
+++ 4-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace _4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создание листа кастом
+            int custom_capacity = int.Parse(Console.ReadLine());
+            ArrayGeneric<int> custom_list = new ArrayGeneric<int>(custom_capacity);
+            // создание дефолтного листа
+            ArrayGeneric<double> default_list = new ArrayGeneric<double>();
+            // добавление элементов
+            custom_list.Add(1);
+            custom_list.Add(2);
+            custom_list.Add(3);
+            // печать элементов
+            foreach(object t in custom_list)
+            {
+                Console.WriteLine(t);
+            }
+            
+        }
+    }
+}
