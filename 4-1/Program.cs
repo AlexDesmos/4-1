38fce6e186ed6dc02b551493bf058b6b47519474
AlexDesmos <154222154+AlexDesmos@users.noreply.github.com>
2024-04-22 20:42:37 +0300

    start

--- 4-1/Program.cs
+++ 4-1/Program.cs
@@ -1,69 +1,23 @@
-﻿using System;
-using System.Collections;
+﻿using Avalonia;
+using System;
 
 namespace _4_1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // создание листа кастом
-            Console.WriteLine("write 'true' if custom, 'false' if default");
-            bool flag = bool.Parse(Console.ReadLine());
-            if (flag)
-            {
-                Console.WriteLine("write size of the array");
-                int custom_capacity = int.Parse(Console.ReadLine());
-                ArrayGeneric<int> custom_list = new ArrayGeneric<int>(custom_capacity);
-                Console.WriteLine("Write elements of the list");
-                for (int i = 0; i < custom_capacity; i++)
-                {
-                    custom_list.Add(int.Parse(Console.ReadLine()));
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
-                }
-                custom_list.Print();
-                Console.WriteLine("First element:");
-                Console.WriteLine(custom_list.Find(t => t % 2 == 0));
-
-                Console.WriteLine("elements > 10");
-                custom_list.Print(custom_list.FindMn(t => t > 10));
-
-                Console.WriteLine("All double elements");
-                custom_list.Print(custom_list.FindMn<double>());
-
-                Console.WriteLine("Write an element you want to find in list");
-                Console.WriteLine(custom_list.Check(int.Parse(Console.ReadLine())));
-
-                Console.WriteLine("Write element to destroy it");
-                custom_list.Remove(int.Parse(Console.ReadLine()));
-                custom_list.Print();
-            }    // создание дефолтного листа
-            else
-            {
-                ArrayGeneric<double> default_list = new ArrayGeneric<double>();
-                Console.WriteLine("Write elements of the list");
-                for (int i = 0; i < 7; i++)
-                {
-                    default_list.Add(int.Parse(Console.ReadLine()));
-                }
-                default_list.Print();
-                Console.WriteLine("First element:");
-                Console.WriteLine(default_list.Find(t => t % 2 == 0));
-
-                Console.WriteLine("elements > 10");
-                default_list.Print(default_list.FindMn(t => t > 10));
-
-                Console.WriteLine("All double elements");
-                default_list.Print(default_list.FindMn<double>());
-
-                Console.WriteLine("Write an element you want to find in list");
-                Console.WriteLine(default_list.Check(int.Parse(Console.ReadLine())));
-
-                Console.WriteLine("Write element to destroy it");
-                default_list.Remove(int.Parse(Console.ReadLine()));
-                default_list.Print();
-            }
-            
-        }
-    }
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
 }
+}
+
